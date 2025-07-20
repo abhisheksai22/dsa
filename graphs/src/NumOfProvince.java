1bcf368bc454824e81941665e746966ea1b40cd9
abhishek <abhisheksai81@gmail.com>
2025-07-20 15:44:10 +0530

    No Of provinces using DFS algo

--- graphs/src/NumOfProvince.java
+++ graphs/src/NumOfProvince.java
@@ -1,8 +1,40 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class NumOfProvince {
 
-    public int countProvinces(ArrayList<ArrayList<Integer>> adj, int size) {
+    public int countProvincesUsingBfs(ArrayList<ArrayList<Integer>> adj, int size) {
+        boolean[] v = new boolean[size];
+        v[0] = true;
+        int count = 0;
+
+        for (int i =1; i < size; i++) {
+            if (!v[i]) {
+                count++;
+                bfs(i, adj, v);
+            }
+        }
+        return count;
+    }
+
+    public void bfs(int i, ArrayList<ArrayList<Integer>> adj, boolean[] v) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        v[i] = true;
+        while(!q.isEmpty()) {
+            Integer node = q.poll();
+            for (Integer it : adj.get(node)) {
+                if(!v[it]) {
+                    q.add(it);
+                    v[it] = true;
+                }
+            }
+        }
+    }
+
+    public int countProvincesUsingDfs(ArrayList<ArrayList<Integer>> adj, int size) {
         boolean[] vis = new boolean[size];
         vis[0] = true;
         int count = 0;
@@ -47,7 +79,9 @@ public class NumOfProvince {
         adj.get(8).add(7);
 
         NumOfProvince n = new NumOfProvince();
-        System.out.println(n.countProvinces(adj, 9));
+        System.out.println("Dfs : " + n.countProvincesUsingDfs(adj, 9));
+        NumOfProvince b = new NumOfProvince();
+        System.out.println("Bfs : " + b.countProvincesUsingBfs(adj, 9));
 
     }
 
